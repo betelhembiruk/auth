9a3a62a028e242688bd0aa06cf23098ba2285478
Betelhem Biruk <betelhembiurk8@gmail.com>
2025-11-11 22:06:37 +0300

    server setup complete

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from "cors"
+import 'dotenv/config'
+import cookieParser from 'cookie-parser';
+import { connect } from 'mongoose';
+import connectDB from './config/mongodb.js'; 
+
+
+const app = express();
+const port = process.env.PORT || 5000
+
+app.use(express.json());
+app.use(cookieParser());
+connectDB();
+app.use(cors({credentials: true}))
+
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+
+app.listen(port, ()=> console.log(`server started on port:${port}`));
